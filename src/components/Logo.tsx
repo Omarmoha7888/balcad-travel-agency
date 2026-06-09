d169b0da116142e41b8333d66cf0cca8d9b8d488
Omarmoha7888 <cumartaakuur7888@gmail.com>
2026-06-09 18:49:32 +0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -12,7 +12,7 @@ export function Logo({ className = '', size = '100%' }: LogoProps) {
       width={size}
       height={size}
       className={`inline-block select-none ${className}`}
-      xmlns="http://www.w3.org/2000/svg"
+      xmlns="logo.png"
     >
       <defs>
         {/* Sun Gradient */}
